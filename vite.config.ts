f2f25e90dbcba338ad72bc2df9d8cbc191d8c1f6
uts4v <utsavstha033@gmail.com>
2026-07-09 13:50:10 +0545

    search bar

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
+// import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
+// export default defineConfig({
+//   nitro: {
+//     preset: "vercel",
+//   },
+//   tanstackStart: {
+//     server: {
+//       entry: "server",
+//     },
+//   },
+//   vite: {
+//     ssr: {
+//       noExternal: ["@supabase/supabase-js"],
+//     },
+//   },
+// });
+
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   nitro: {
-    preset: "vercel",
+    preset: "node-server",
   },
   tanstackStart: {
     server: {
